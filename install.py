c66ef783c86b6d7d0edf1352380cdac5ab7f3045
Huyston <williamscaff.usp@gmail.com>
2021-04-16 18:33:06 -0300

    Added install script

--- install.py
+++ install.py
@@ -1,1 +1,17 @@
 # Photon installer script
+
+import os
+import sys
+
+with open('photon.py') as w:
+    code = w.read()
+
+code = code.replace('@PHOTON_INSTALL_PATH',f'"{os.getcwd()}/"')
+
+if sys.platform in {'linux','darwin'}:
+    with open('/usr/local/bin/photon','w') as w:
+        w.write(code)
+
+    os.chmod('/usr/local/bin/photon',0o777)
+else:
+    print('Automatic installation in this system is not supported yet.')
