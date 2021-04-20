36505d839bfbc9ea3cb01e894afaad8d52df30a6
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-04-20 00:35:14 -0300

    Não foi mudado nada.

--- interpreter.py
+++
@@ -1,7 +0,0 @@
-# Photon Interpreter
-#
-# This script funcion is:
-#   - Get lines of code from a file or interpreter
-#   - Call the parser to generate tokens and the struct
-#   - Call the engine to Process the struct
-#   - Run the processed struct
