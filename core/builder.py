9ad5f68ea5c372fa67ee3f51d9fd07822fb0e245
William Scaff <williamscaff.usp@gmail.com>
2021-04-21 20:39:02 -0300

    Added placeholder classes

--- core/builder.py
+++ core/builder.py
@@ -2,3 +2,8 @@
 # This script is used to generate a Photon project
 # according to the target platform and preferences
 # Each target is handled by its Toolchain
+
+class Builder():
+    def __init__(self, **kwargs):
+        pass
+
--- core/interpreter.py
+++ core/interpreter.py
@@ -5,3 +5,10 @@
 #   - Call the parser to generate tokens and the struct   
 #   - Call the engine to Process the struct
 #   - Run the processed struct
+
+class Interpreter():
+    def __init__(self, filename, **kwargs):
+        pass
+
+    def run(self):
+        print('Not implemented yet. Please be patient.')
