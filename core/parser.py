98c2ae636e082e0911645b4e7abae778a5ae7272
William Scaff <williamscaff.usp@gmail.com>
2021-04-21 22:11:06 -0300

    Added interpreter code

--- core/parser.py
+++ core/parser.py
@@ -2,3 +2,9 @@
 # This script converts lines of photon code into tokens
 # and also generates the struct of the code.
 # This struct is used by the Engine to execute the code.
+
+def parse():
+    pass
+
+def assembly():
+    pass
