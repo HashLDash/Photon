4c3a2738db9afaea9f8eaee77550399e227427f1
William Scaff <williamscaff.usp@gmail.com>
2021-04-27 09:40:28 -0300

    added var init tests

--- tests/parserTest.py
+++ tests/parserTest.py
@@ -19,30 +19,48 @@ class ParserTest(unittest.TestCase):
             struct, nextLine = i.handleTokenized(tokenized)
         return struct
         
-    def test_printFunc1(self):
-        struct = self.runFile('/printFunc/printString.w')
+    def test_printStr(self):
+        struct = self.runFile('/printFunc/printStr.w')
         self.assertEqual(struct['token'], 'printFunc')
         self.assertEqual(struct['expr']['args'][0], {'type':'str', 'value':'"Hello World"'})
 
-    def test_printFunc2(self):
+    def test_printInt(self):
         struct = self.runFile('printFunc/printInt.w')
         self.assertEqual(struct['token'], 'printFunc')
         self.assertEqual(struct['expr']['args'][0], {'type':'int', 'value':'26'})
 
-    def test_printFunc3(self):
+    def test_printFloat(self):
         struct = self.runFile('printFunc/printFloat.w')
         self.assertEqual(struct['token'], 'printFunc')
         self.assertEqual(struct['expr']['args'][0], {'type':'float', 'value':'4.7'})
 
-    def test_printFunc4(self):
+    def test_printFloatDot(self):
         struct = self.runFile('printFunc/printFloatDot.w')
         self.assertEqual(struct['token'], 'printFunc')
         self.assertEqual(struct['expr']['args'][0], {'type':'float', 'value':'2.'})
 
-    def test_printFunc5(self):
+    def test_printVar(self):
         struct = self.runFile('printFunc/printVar.w')
         self.assertEqual(struct['token'], 'printFunc')
         self.assertEqual(struct['expr']['args'][0]['name'], 'var')
+
+    def test_varInitInt(self):
+        struct = self.runFile('varInit/initInt.w')
+        self.assertEqual(struct['token'], 'expr')
+        self.assertEqual(struct['args'][0]['name'], 'a')
+        self.assertEqual(struct['args'][0]['type'], 'int')
+
+    def test_varInitFloat(self):
+        struct = self.runFile('varInit/initFloat.w')
+        self.assertEqual(struct['token'], 'expr')
+        self.assertEqual(struct['args'][0]['name'], 'a')
+        self.assertEqual(struct['args'][0]['type'], 'float')
+
+    def test_varInitStr(self):
+        struct = self.runFile('varInit/initStr.w')
+        self.assertEqual(struct['token'], 'expr')
+        self.assertEqual(struct['args'][0]['name'], 'a')
+        self.assertEqual(struct['args'][0]['type'], 'str')
 
 if __name__ == "__main__":
     unittest.main()
