4eef51880b8e96d116d87bc37ba5f88b9c1ce349
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2021-05-06 01:29:08 -0300

    check before adding to PATH, remove inputs and add prints

--- install.py
+++ install.py
@@ -16,26 +16,28 @@ try:
             os.remove(p_path)
         os.symlink(PHOTON_PATH, p_path)
         os.chmod(PHOTON_PATH, 0o777)
+        print(f'Photon Install Path: {p_path}')
     elif sys.platform in {'win32', 'cygwin', 'msys'} or os.name == "nt" or os.environ.get('OS', '') == 'Windows_NT':
         p_dir = os.path.expandvars('%ProgramFiles%\\Photon')
         if not os.path.exists(p_dir):
             os.mkdir(p_dir)
         with open(os.path.join(p_dir, 'photon.bat'), 'w') as w:
             w.write(f'@echo off\npython "{PHOTON_PATH}" %*')
-        os.system(f'setx /M PATH "%PATH%;{p_dir}"')
+        if p_dir in os.environ.get("PATH"):
+            print('Photon already on PATH, skipping')
+        else:
+            os.system(f'setx /M PATH "%PATH%;{p_dir}"')
+        print(f'Photon Install Path: {os.path.join(p_dir, "photon")}')
     else:
         print('Automatic installation in this system is not supported yet.')
-        input('Press enter to close')
         sys.exit(1)
 except PermissionError:
     print('The installation failed due to lack of permissions.',
           'Run the proper script for your operational system or try to execute this as super user/administrator',
           sep='\n')
-    input('Press enter to close')
     sys.exit(1)
 
 if not haveDependencies('c', sys.platform):
     resolveDependencies('c', sys.platform)
 
 print("Successfully installed! Now you can use the photon command!")
-input('Press enter to close and play with Photon!')
