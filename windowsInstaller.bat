50fd092bac1395ebc64aac6d897e176c83db83fe
matheusjohannaraujo <matheusjohannaraujo@gmail.com>
2021-05-08 19:49:37 -0300

    Improved Windows installer!

--- windowsInstaller.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-
-SET mydir=%~dp0
-powershell -command "Start-Process powershell -Verb RunAs -PassThru -ArgumentList \"-ExecutionPolicy RemoteSigned -NoProfile -File %mydir%\windowsInstaller.ps1\""
