7429c738ec3d7996bedfe4583c93188bb9a91ebb
matheusjohannaraujo <matheusjohannaraujo@gmail.com>
2021-05-08 20:10:05 -0300

    Improved Windows installer!

--- windowsInstaller.bat
+++ windowsInstaller.bat
@@ -1,4 +1,6 @@
-@ECHO OFF
-
-SET mydir=%~dp0
+@echo off
+title Windows Installer - Photon
+set mydir=%~dp0
 powershell -command "Start-Process powershell -Verb RunAs -PassThru -ArgumentList \"-ExecutionPolicy RemoteSigned -NoProfile -File %mydir%\windowsInstaller.ps1\""
+timeout /t 3
+exit
