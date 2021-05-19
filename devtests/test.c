69e028950f8b41f32bccacc3712bc66b44f674dc
Huyston <williamscaff.usp@gmail.com>
2021-05-19 16:58:49 -0300

    Clean repo

--- devtests/test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char* a;
-    int len = asprintf(&a, "%s", "Hello");
-    printf("%s\n", a);
-}
--- devtests/testMultiplatform.c
+++
@@ -1,8 +0,0 @@
-#include "asprintf.h"
-#include <stdio.h>
-
-int main() {
-    char* a;
-    int len = asprintf(&a, "%s", "Hello");
-    printf("%s\n", a);
-}
