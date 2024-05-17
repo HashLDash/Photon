003b5552fbfd7aa5fe5b913eb3d54da9f77214b7
Huyston <william.scaff@cromai.com>
2024-05-17 20:44:38 -0300

    Update version

--- core/photon.py
+++ core/photon.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     from interpreter import Interpreter
     from builder import Builder
     from dependencies import haveDependencies, resolveDependencies
-    __version__ = '0.0.7'
+    __version__ = '0.0.9'
     try:
         if '-d' in sys.argv:
             sys.argv.remove('-d')
